4b5b99c8ee6e483c3330a3f484598f56af71a8bf
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-05-27 02:16:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+  }, [])
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? 'Loading...' : data}</p>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
