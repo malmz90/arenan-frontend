02fae3de45258100d777e688a6e9a658850d42c9
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-08-02 14:34:41 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'
 import { useState, useEffect } from 'react'
 import './App.css'
 
@@ -6,15 +5,14 @@ function App() {
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/api')
+    fetch('http://localhost:4000/api')
       .then((res) => res.json())
-      .then((data) => setData(data.message))
+      .then((data) => console.log(data))
   }, [])
   console.log(data)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? 'Loading...' : data}</p>
       </header>
     </div>
