a9aced3afcc677f1ad63d3c61b397070e496868a
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-09-06 18:49:54 +0200

    logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,56 @@
+import { useNavigate } from 'react-router-dom'
+import { removeUser } from '../redux/reducers/user'
+import { useDispatch, useSelector } from 'react-redux'
 const Header = () => {
+  const user = useSelector((state) => state.user.user)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  console.log('header', user)
+  const logout = () => {
+    fetch('http://localhost:4000/logout', {
+      method: 'GET',
+      credentials: 'include',
+    })
+    navigate('/')
+    dispatch(removeUser())
+  }
+
   return (
-    <ul class="flex">
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="#">
-          Active
-        </a>
-      </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="#">
-          Link
-        </a>
-      </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="#">
-          Link
-        </a>
-      </li>
-      <li class="mr-6">
-        <a class="text-gray-400 cursor-not-allowed" href="#">
-          Disabled
-        </a>
-      </li>
+    <ul class="flex flex-row justify-between">
+      <div className="flex flex-row">
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">
+            Active
+          </a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">
+            Link
+          </a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">
+            Link
+          </a>
+        </li>
+        <li class="mr-6">
+          <a class="text-gray-400 cursor-not-allowed" href="#">
+            Disabled
+          </a>
+        </li>
+      </div>
+      <div className="justify-end">
+        {user && (
+          <li>
+            <button
+              onClick={logout}
+              className="text-xs font-medium text-gray-500 group-hover:text-gray-200"
+            >
+              Logout
+            </button>
+          </li>
+        )}
+      </div>
     </ul>
   )
 }
